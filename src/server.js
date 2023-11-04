c0c25516fb91402285a44ba47a9e03e2ec81f9cd
Carlos Afonso <carlos.flach15@gmail.com>
2023-11-04 15:45:02 +0100

    feat: add json middleware to applicaiton

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 import http from 'node:http';
+import { json } from './middlewares/json.js';
 const PORT = 3333;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
+	const { method, url } = req;
+
+	await json(req, res);
+
 	res.writeHead(200).end('Hello from the server');
 });
 
