07951ec603f410b29579da2985df931baa795452
Carlos Afonso <carlos.flach15@gmail.com>
2023-11-04 16:30:55 +0100

    chore: reestructure folder directory

---
+++ src/routes.js
@@ -0,0 +1,46 @@
+import { randomUUID } from 'node:crypto';
+import { Database } from './database.js';
+import { buildRoutePath } from './utils/build-path-route.js';
+
+const database = new Database();
+
+export const routes = [
+	{
+		method: 'POST',
+		path: buildRoutePath('/tasks'),
+		handler: (req, res) => {
+			const { title, description } = req.body;
+			const task = {
+				id: randomUUID(),
+				title,
+				description,
+				created_at: Date.now(),
+				completed_at: null,
+				updated_at: null,
+			};
+
+			database.insert('tasks', task);
+
+			return res.writeHead(201).end(JSON.stringify(task));
+		},
+	},
+	{
+		method: 'GET',
+		path: buildRoutePath('/tasks'),
+		handler: (req, res) => {
+			const { title, description } = req.query;
+
+			const users = database.select(
+				'tasks',
+				title || description
+					? {
+							title: title,
+							description: description,
+					  }
+					: null
+			);
+
+			return res.end(JSON.stringify(users));
+		},
+	},
+];
